7d90e98b17b930870f8d8b503e50ad07780bd2d5
Iris <iris.thuo@student.moringaschool.com>
2023-01-15 23:17:08 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+
+function App(){
+    const [randoDog, setrandoDog] = useState([])
+    const [gettingDoggos, setgettingDoggos] = useState(false)
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            setrandoDog(data.message)
+            setgettingDoggos(true)
+        })
+    })
+    if (!gettingDoggos) return <p>Loading...</p>;
+
+    return(
+        <div>
+            <img src={randoDog} alt="Cute Doggos" />
+        </div>
+    )
+}
+
+export default App
